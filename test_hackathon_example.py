a4c6e29b65ab965fdc9d53e04253bd892a1839cf
Mario Wendt <mario.wendt@desy.de>
2019-01-29 16:08:44 +0100

    initial commit

--- test_hackathon_example.py
+++ test_hackathon_example.py
@@ -2,29 +2,9 @@ from hackathon_example import greeting, parse_args, main
 import pytest
 
 
-def test_greeting():
-    assert greeting("John") == "Hello, John!"
+def test_addit():
+    assert result(3) == 6
 
-
-def test_greeting_en():
-    assert greeting("John", "en") == "Hello, John!"
-
-
-def test_greeting_de():
-    assert greeting("John", "de") == "Hallo, John!"
-
-
-def test_greeting_unknown_lang():
-    with pytest.raises(ValueError):
-        greeting("John", "fr")
-
-
-@pytest.mark.parametrize("argv, lang, name", [
-    (["John"], "en", "John"),
-    (["-l", "en", "John"], "en", "John"),
-    (["-l", "de", "John"], "de", "John"),
-    (["John Doe"], "en", "John Doe"),
-])
 def test_parse_args(argv, lang, name):
     args = parse_args(argv)
     assert args.name == name
